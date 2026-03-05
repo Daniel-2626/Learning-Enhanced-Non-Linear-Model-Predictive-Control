8f0c1fae289672d3863caffce90f1e6eed459c80
LechKula <lech.kula@outlook.com>
2026-03-05 15:31:12 +0100

    Add files via upload

---
+++ adaptive_nn_mpc_battery.py
@@ -0,0 +1,318 @@
+import casadi as cs
+import numpy as np
+import torch
+import torch.nn as nn
+import l4casadi as l4c
+from acados_template import AcadosOcpSolver, AcadosOcp, AcadosModel
+import time
+import scipy.linalg
+import matplotlib.pyplot as plt
+import pandas as pd
+import os 
+from datetime import datetime
+import ctypes
+import sys
+
+
+CELSIUS_TO_KELVIN = 273.15
+
+class MLP(nn.Module):
+    def __init__(self, input_dim=2, output_dim=1, hidden_dim=128, num_layers=3):
+        super(MLP, self).__init__()
+        layers = [nn.Linear(input_dim, hidden_dim), nn.ReLU()] # nn.ReLU rectified linear function (max(x,0))
+        for _ in range(num_layers - 1):
+            layers.extend([nn.Linear(hidden_dim, hidden_dim), nn.ReLU()]) # nn.Linear applies an affine transform. hidden_dim features and hidden_dim out features
+        layers.append(nn.Linear(hidden_dim, output_dim))
+        self.net = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.net(x)
+
+class BatteryLearnedDynamics:
+    def __init__(self, residual_model): # remove gym_env for cascaded tank
+        self.residual_model = residual_model # Residual model is L4Casadi residual
+    
+    def model(self): # remove gym_env for cascaded tank
+
+        T_bat = cs.MX.sym('Tb')
+        SOC= cs.MX.sym('SOC')
+        X = cs.vertcat(T_bat, SOC)
+        omega = cs.MX.sym('omega') # Power into heater/cooler given as efficiency*Pin
+        Q_heat = cs.MX.sym('Q_heat') # Pump control (rpm that is converted to kg/s)
+        current = cs.MX.sym('current')
+        U = cs.vertcat(omega, Q_heat)
+        nx = 2
+        nu = 2
+
+        # Parameters
+        m_battery = 20*2.5*4
+        c_battery = 795
+        c_coolant = 3500
+        density_coolant = 1050
+        pump_displacement = 1/(2*np.pi)*40/(100**3) # D parameter in simulink
+        R_battery = 4*20*0.0128 # Battery resistance
+        C_battery = 28*3600 # in coloumb
+        hA_bat = 2500
+        alpha = 0.65
+        kappa = 1e-4
+        mdot_c = density_coolant*pump_displacement*omega
+        
+
+
+        # Dynamics
+        
+        # Get the cooler in and out temps
+        #NTU_bat  = (alpha_out*hb*Ab) / (u2*c_clin + 1e-3)
+        #T_clin= Tb + 1/(1-np.exp(-NTU_bat))*u1/(u2*c_clin + 1e-3)
+        #T_clout = (T_clin - Tb) * np.exp(-NTU_bat) + Tb
+        #Q_cool = u2*c_clin*(T_clout - T_clin)
+
+        # Now for the actual calculations 
+        T_bat_dot_model = alpha/(m_battery*c_battery) * (current**2 * R_battery + mdot_c * kappa * Q_heat)
+        SOC_dot_model = -current/C_battery
+        X_dot_nominal = cs.vertcat(T_bat_dot_model, SOC_dot_model)
+
+        mlp_input = cs.vertcat(X, U)
+        residual = self.residual_model(mlp_input.T).T 
+        X_dot_residual = cs.vertcat(residual[0], residual[1]) # x1 dot and x2 dot residual
+
+        f_expl = X_dot_nominal + X_dot_residual # adding x dot residual leads to some stochasticity, maybe because we have a arbitrary neural network initially?
+        x_start = np.array([-10+CELSIUS_TO_KELVIN,1]) # initial constraint (gets overwritten)
+
+        # store to struct
+        model = cs.types.SimpleNamespace()
+        model.x = X
+        model.xdot = cs.MX.sym('xdot', 2)
+        model.u = U
+        model.z = cs.vertcat([])
+        model.p = cs.vertcat(current)
+        model.f_expl = f_expl
+        model.f_nominal = X_dot_nominal
+        model.x_start = x_start
+        model.constraints = cs.vertcat([]) # add constraints here or in mpc?
+        model.name = "battery_learned"
+        
+        return model 
+
+class MPC:
+    def __init__(self, model, N, t_horizon, external_shared_lib_dir, external_shared_lib_name):
+        self.model = model
+        self.N = N
+        self.t_horizon = t_horizon
+        self.external_shared_lib_dir = external_shared_lib_dir
+        self.external_shared_lib_name = external_shared_lib_name
+
+    @property # a decorator
+    def solver(self):
+        return AcadosOcpSolver(self.ocp())
+    
+    def ocp(self):
+        model = self.model
+
+        t_horizon = self.t_horizon
+        N = self.N
+
+        # Get model
+        model_ac = self.acados_model(model=model)
+
+        # Dimensions
+        nx = 2
+        nu = 2
+        ny = nx + nu # Stage cost
+        ny_e = nx # Terminal cost considers only states
+
+        # Create ocp to formulate optim
+        ocp = AcadosOcp()
+        ocp.model = model_ac
+        ocp.dims.N = N
+        ocp.dims.nx = nx
+        ocp.dims.nu = nu
+        ocp.dims.ny = ny
+        ocp.solver_options.tf = t_horizon
+
+        # initialize cost function
+        ocp.cost.cost_type = 'LINEAR_LS'
+        ocp.cost.cost_type_e = 'LINEAR_LS'
+
+        # state 
+        ocp.cost.Vx = np.zeros((ny, nx)) # i feel like this should be nx,nx ||Vx x||^2 
+        for i in range(nx):
+            ocp.cost.Vx[i,i] = 1 # so set the matrix coeff corr to u to 0. maybe the dim of Vx is set to be the same as Vu
+        ocp.cost.Vu = np.zeros((ny, nu))
+        for i in range(nu):
+            ocp.cost.Vu[i + nx, i] = 1
+        ocp.cost.Vz = np.array([[]]) # don't know what the V_z z, what the variable z should be
+        ocp.cost.Vx_e = np.eye(nx)
+        l4c_y_expr = None
+
+        # Define weight parameters
+        Q = 1e0 * np.diag([100000, 0])
+        R = 1e-5 * np.diag([1, 0.1])
+        ocp.cost.W = scipy.linalg.block_diag(Q,R)
+
+        # Initial state (will be overwritten?)
+        ocp.cost.W_e = Q 
+        ocp.cost.yref = np.zeros((ny, ))
+        ocp.cost.yref_e = np.zeros((ny_e, ))
+
+        # Initial state
+        ocp.constraints.x0 = model.x_start
+
+        # Set constraints
+        omega_max = 4000*2*np.pi/60
+        omega_min = 10
+        Q_heat_max = 4000
+        Q_heat_min = 0
+        Tb_max = 50 + CELSIUS_TO_KELVIN
+        Tb_min = -20 + CELSIUS_TO_KELVIN
+        SOC_max = 1
+        SOC_min = 0
+        ocp.constraints.lbu = np.array([omega_min, Q_heat_min])
+        ocp.constraints.ubu = np.array([omega_max, Q_heat_max])
+        ocp.constraints.idxbu = np.array([0,1])
+        ocp.constraints.idxbx = np.array([0]) # at what indices to have constraints? 
+        ocp.constraints.ubx = np.array([Tb_max])
+        ocp.constraints.lbx = np.array([Tb_min])
+
+        # Solver options
+        ocp.solver_options.qp_solver = "FULL_CONDENSING_HPIPM"
+        ocp.solver_options.hessian_approx = "GAUSS_NEWTON"
+        ocp.solver_options.integrator_type = "ERK"
+        ocp.solver_options.nlp_solver_type = "SQP_RTI"
+        ocp.solver_options.model_external_shared_lib_dir = self.external_shared_lib_dir
+        ocp.solver_options.model_external_shared_lib_name = self.external_shared_lib_name 
+
+        return ocp
+
+    def acados_model(self, model):
+        model_ac = AcadosModel()
+        model_ac.f_impl_expr = model.xdot - model.f_expl # Implicit 0 = xdot - f
+        model_ac.f_expl_expr = model.f_expl
+        model_ac.x = model.x
+        model_ac.xdot = model.xdot
+        model_ac.u = model.u
+        model_ac.p = model.p
+        model_ac.name = model.name
+        return model_ac
+
+
+
+class Controller:
+    def setup(self):
+        # Residual MLP: Lightweight
+        residual_mlp = MLP(input_dim = 2 + 2, output_dim=2, hidden_dim=64, num_layers=3) # the network
+        for param in residual_mlp.parameters():
+            param.requires_grad = False
+        self.residual_mlp = residual_mlp
+        self.residual_optimizer = torch.optim.Adam(residual_mlp.parameters(), lr=1e-3) # lr = learning rate, the optimizer
+        self.residual_criterion = nn.MSELoss()
+
+        l4c_residual = l4c.L4CasADi(self.residual_mlp, name="battery", mutable=True)
+        self.l4c_residual = l4c_residual
+        
+        # MPC Setup 
+        N = 40
+        t_horizon = 40
+        learned_model = BatteryLearnedDynamics(l4c_residual)
+      
+        casadi_model = learned_model.model()
+
+        
+        self.solver = MPC(model=learned_model.model(), N=N, t_horizon = t_horizon,
+                    external_shared_lib_dir=l4c_residual.shared_lib_dir,
+                    external_shared_lib_name=l4c_residual.name).solver # Returns the solver object from MPC
+
+         
+        
+
+        # SOC_ref = None # This is not actually tracked
+        self.obs_buffer = []
+        self.batch_size = N
+        self.T_update = N
+        self.current_iterate = 0
+        self.xt_pred = np.array([CELSIUS_TO_KELVIN, 1])
+
+    def get_input(self, T_bat_target, T_bat_0, SOC_0, current_0):
+        if self.current_iterate == 0:
+            self.xt_pred([T_bat_0, SOC_0])
+
+        # Set reference for each step in MPC horizon
+        for k in range(N):
+            #if k == 0: # only store first reference
+                #Tb_ref_history.append(Tb_ref)
+                # SOC_ref_history.append(SOC_ref)
+            # Set terminal reference
+            # y_ref_k = np.array([Tb_ref, SOC_ref, 0])
+            y_ref_k = np.array([Tb_ref, 0.0])
+            self.solver.set(k, "yref", y_ref_k)
+            self.solver.set(k, "p", current_0)
+
+        # Set terminal reference
+        # y_ref_terminal = np.array([Tb_ref, SOC_ref])
+        y_ref_terminal = np.array([Tb_ref, 0.0])
+        self.solver.set(N, "yref", y_ref_terminal)
+
+        start = time.time()
+        xt = np.array([T_bat_0,SOC_0])
+      
+        # Apply current state as constraint
+        self.solver.set(0, "lbx", xt)
+        self.solver.set(0, "ubx", xt)
+
+        # Solve mpc and apply control
+        self.solver.solve()
+        # ut = solver.get(0, "u").item()
+        ut = self.solver.get(0, "u")
+        omega_value = ut[0].item()
+        Q_heat_value = ut[1].item()
+        
+        
+        self.obs_buffer.append((xt[0], xt[1], omega_value, Q_heat_value, self.xt_pred[0], self.xt_pred[1]))
+        # sl = solver.get(1, "sl")
+        # su = solver.get(1, "su")
+        # print(sl,su)
+        
+        # Want to compare prediction at time step 0 with value at time step 1
+        # So dealy update of xt pred
+        pred = self.solver.get(1, 'x')
+        self.xt_pred = np.array[pred[0].item(), pred[1].item()]
+     
+        
+        
+        # update every 50 time steps
+        if self.current_iterate > 0 and (self.current_iterate % self.T_update) == 0 and len(self.obs_buffer) >= self.batch_size:
+            data = np.array(self.obs_buffer[-self.batch_size:])
+            # data[:, :3] h1, h2 and u
+            X_batch = torch.tensor(data[:, :4], dtype=torch.float32)
+            # h1_dot, h2_dot
+            #print(data[-1])
+            y_true = data[:, :2]
+            #print(y_true)
+        
+            
+
+            y_nominal = data[:, -2:]
+            #y_true = np.array([f(x[:2], x[3]).full().flatten() for x in data])
+            #y_nominal = nominal[:, :] # dx2 and dx4 from nominal model
+            #print(y_nominal.shape)
+            #print(y_nominal)
+            #print(y_true.shape)
+            #print(y_true)
+            #print(y_true - y_nominal)
+            #stop
+            y_target = torch.tensor(y_true - y_nominal, dtype=torch.float32)
+
+            for p in self.residual_mlp.parameters(): p.requires_grad = True
+            for _ in range(50):
+                self.residual_optimizer.zero_grad() # optimizer object
+                prediction = self.residual_mlp(X_batch) # gives data to network to make a prediction
+                loss = self.residual_criterion(prediction, y_target)
+                loss.backward() # calculates gradient
+                self.residual_optimizer.step() # one optimization step to update parameters
+            for p in self.residual_mlp.parameters(): p.requires_grad = False
+            self.l4c_residual.update(residual_mlp)
+
+        elapsed = time.time() - start
+        print(elapsed, 'ms')
+        self.current_iterate += 1
+        return omega_value, Q_heat_value, self.xt_pred[0]
---
+++ nominal_model_predictive_control.py
@@ -0,0 +1,230 @@
+import casadi as ca
+import numpy as np
+import matplotlib.pyplot as plt
+from time import time
+import pandas as pd
+class Model_Predictive_Controller:
+    def MPC_setup(self, T_bat_target):
+
+        ## CONFIG
+        # MPC setup
+        scale_omega = 100000
+        scale_q_heat = 10000
+        celsius_to_kelvin = 273.15
+
+        Q_T_bat = 1*scale_q_heat*scale_omega
+        Q_SOC = 0 # Don't care what SOC is. Might cause problems in the future
+        
+        R_omega = 1/scale_omega
+        R_Q_heat = 1/scale_q_heat
+        t_horizon = 40
+        N = 40 # number of look ahead steps
+        step_horizon = t_horizon/N # time between steps in seconds
+
+        # Constraints
+        T_bat_max = 100 + celsius_to_kelvin
+        T_bat_min = -100 + celsius_to_kelvin
+        SOC_min = 0
+        SOC_max = 1
+        omega_max = 4000*2*np.pi/60
+        omega_min = 1000*2*np.pi/60 
+
+        Q_heat_max = 4000 # W
+        Q_heat_min = 0 # W
+        # Target
+        T_bat_target = T_bat_target
+        SOC_target = 0 # No target actually
+        # State symbolic variables
+        T_bat = ca.SX.sym('T_bat')
+        SOC = ca.SX.sym('SOC')
+        states = ca.vertcat(
+            T_bat,
+            SOC
+        )
+        n_states = states.numel() # Returns amount of elements
+
+        # control symbolic variables
+        omega = ca.SX.sym('omega')
+        Q_heat = ca.SX.sym('Q_heat')
+
+        controls = ca.vertcat(
+            omega,
+            Q_heat
+        )
+        n_controls = controls.numel()
+
+        # disturbances 
+        current = ca.SX.sym("I_bat")
+        disturbances = ca.vertcat(current)
+        n_disturbances = disturbances.numel()
+
+        # matrix containing all states over all time steps
+        X = ca.SX.sym('X', n_states, N+1)
+
+        # matrix containing all control actions over all time steps
+        U = ca.SX.sym('U', n_controls, N)
+
+        # column vector for storing initial state, target state AND disturbances 
+        P = ca.SX.sym('P', n_states + n_states + n_disturbances)
+
+        # State weights matrix
+        Q = ca.diagcat(Q_T_bat, Q_SOC)
+
+        # Controls weight matrix
+        R = ca.diagcat(R_omega, R_Q_heat)
+
+        # discretization model (e.g. x2 = f(x1, v, t) = x1 + v*dt)
+        # Parameters
+        m_battery = 20*2.5*4
+        c_battery = 795
+        c_coolant = 3500
+        density_coolant = 1050
+        pump_displacement = 1/(2*np.pi)*40/(100**3) # D parameter in simulink
+        R_battery = 4*20*0.0128 # Battery resistance
+        C_battery = 28*3600 # in coloumb
+        hA_bat = 2500
+        alpha = 0.65
+
+        mdot_c = density_coolant*pump_displacement*omega
+        NTU_bat = hA_bat/(mdot_c*c_coolant + 1e-3)
+        T_coolant_in =  T_bat + 1/(1-ca.exp(-NTU_bat)+1e-3)*Q_heat/(mdot_c * c_coolant + 1e-3)
+        T_coolant_out = (T_coolant_in - T_bat) * ca.exp(-NTU_bat) + T_bat
+        
+        Q_cool = mdot_c * c_coolant * (T_coolant_out-T_coolant_in)
+        ## Controller model 
+        T_bat_dot_model = alpha/(m_battery*c_battery) * (current**2 * R_battery - Q_cool)
+        SOC_dot_model = -current/C_battery
+        ode_model = ca.vertcat(T_bat_dot_model, SOC_dot_model)
+
+        f_model = ca.Function("f_model", [states,controls,disturbances], [ode_model], ["x", "u", "d"], ["ode"])
+        
+        cost_fn = 0 # Cost function
+        g = X[:, 0] - P[:n_states] # constraints in the equation, that x0 = p0 
+
+        # Runge Kutta
+        for k in range(N):
+            # model update, what our controller knows
+            st = X[:, k]
+            contr = U[:, k]
+            disturbances = P[-n_disturbances:]
+
+            cost_fn = (cost_fn +
+                    (st-P[n_states:2*n_states]).T @ Q @ (st-P[n_states:2*n_states]) +
+                    contr.T @ R @ contr
+                    )
+            st_next = X[:, k+1]
+            K1 = f_model(st, contr, disturbances)
+            K2 = f_model(st + step_horizon/2 * K1, contr, disturbances)
+            K3 = f_model(st + step_horizon/2 * K2, contr, disturbances)
+            K4 = f_model(st + step_horizon * K3, contr, disturbances)
+            st_next_RK4 = st + (step_horizon / 6) * (K1 + 2*K2 + 2*K3 + K4)
+            g = ca.vertcat(g, st_next - st_next_RK4) # Basically saying X[:, k+1] = runge kutta evaluation
+        cost_fn = cost_fn +  (X[:, N]-P[n_states:2*n_states]).T @ Q @ (X[:, N]-P[n_states:2*n_states])
+        OPT_variables = ca.vertcat(
+            X.reshape((-1,1)),    # E.g. 3 x 11 --> 33 x 1
+            U.reshape((-1,1))     # E.g. 4 x 10 --> 40 x 1
+        )
+        nlp_prob = {
+            'f': cost_fn,
+            'x': OPT_variables,
+            'g': g,
+            'p': P
+        }
+
+        opts = {
+            'ipopt': {
+                'max_iter': 2000,
+                'print_level': 0,
+                'acceptable_tol': 1e-8,
+                'acceptable_obj_change_tol': 1e-6
+            },
+            'print_time': 0
+        }
+
+        solver = ca.nlpsol('solver', 'ipopt', nlp_prob, opts)
+
+        lbx = ca.DM.zeros((n_states*(N+1) + n_controls*N, 1))
+        ubx = ca.DM.zeros((n_states*(N+1) + n_controls*N, 1)) # long column vector 
+
+        lbx[0: n_states*(N+1): n_states] = T_bat_min # Different notation to matlab. Here have [start:stop.step]. We fill out the vector related to states [start:n_states*(N+1)] with a lower bound every n_states, meaning we set lower bound for first state at each time step
+        lbx[1: n_states*(N+1): n_states] = SOC_min # Here set lower bound for second state
+
+        ubx[0: n_states*(N+1): n_states] = T_bat_max 
+        ubx[1: n_states*(N+1): n_states] = SOC_max 
+
+        lbx[n_states*(N+1): -1: n_controls] = omega_min 
+        ubx[n_states*(N+1): -1: n_controls] = omega_max 
+        
+        lbx[1+ n_states*(N+1): -1: n_controls] = Q_heat_min 
+        ubx[1+ n_states*(N+1): -1: n_controls] = Q_heat_max 
+
+        
+        
+
+        args = {
+            'lbg': ca.DM.zeros((n_states*(N+1), 1)), # constrainst lower bound (basically giving == constraint)
+            'ubg': ca.DM.zeros((n_states*(N+1), 1)),
+            'lbx': lbx, 
+            'ubx': ubx
+        }
+
+        self.solver = solver
+        self.args = args
+        self.state_target = ca.DM([T_bat_target, SOC_target])
+
+        self.u0 = 1000*ca.DM.ones((n_controls, N))
+        self.X0 = 1000*ca.DM.ones((n_states, N+1))
+
+        self.n_states = n_states
+        self.n_controls = n_controls
+        self.N = N
+
+    def DM2Arr(self,dm):
+    # returns a full matrix instead if a sparse one
+        return np.array(dm.full())
+    def MPC_step(self, T_bat_0, SOC_0, current):
+        start = time()
+        state_init = ca.DM([T_bat_0, SOC_0])
+        state_target = self.state_target
+        print('state init', state_init, 'state target', state_target)
+        X0 = self.X0
+        u0 = self.u0
+        args = self.args
+        solver = self.solver
+        n_states = self.n_states
+        n_controls = self.n_controls
+        N = self.N
+        args['p'] = ca.vertcat(
+            state_init, # current state
+            state_target,
+            current 
+        )
+
+        # optimization variable current state
+        args['x0'] = ca.vertcat(
+            ca.reshape(X0, n_states*(N+1), 1),
+            ca.reshape(u0, n_controls*N, 1)
+        )
+        # sol.x0 is warm starting it
+        sol = solver(
+            x0 = args['x0'],
+            lbx = args['lbx'],
+            ubx = args['ubx'],
+            lbg = args['lbg'],
+            ubg = args['ubg'],
+            p = args['p']
+        )
+        
+        self.u0 = ca.reshape(sol['x'][n_states*(N+1):], n_controls, N   ) # gives u as a vector where rows are the different inputs and columns the time steps (applied input at col 0)
+        self.X0 = ca.reshape(sol['x'][:n_states*(N+1)], n_states, N+1)
+
+        inp = self.DM2Arr(self.u0[:, 0])
+        omega = inp[0].item()
+        Q_heat = inp[1].item()
+        X_predict = self.DM2Arr(self.X0[:, 1])
+        T_bat_predict = X_predict[0].item()
+        end = time()
+        print(end - start, "s")
+        print(omega, Q_heat)
+        return omega, Q_heat, T_bat_predict
+        
